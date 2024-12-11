5429ed41ada75d446b2d4f0321955fd8eda37878
aporosh <115151056+aporosh@users.noreply.github.com>
2024-12-11 18:19:24 +0300

    update chromedriver

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -19,7 +19,7 @@ public abstract class BaseTest {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver-linux64/chromedriver");
         //System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver-win64/chromedriver.exe");
         //System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromeDriver\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        //System.setProperty("webdriver.chrome.driver", "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_130");
+        //"https://chromedriver.storage.googleapis.com/LATEST_RELEASE_130");
         driver = new ChromeDriver(options);
        // driver.manage().window().maximize();
        //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
