743f86a930f34ddfedbdb1bb745b3b8bf7c03123
aporosh <115151056+aporosh@users.noreply.github.com>
2025-04-13 22:31:18 +0300

    test commit

--- src/main/java/api/PaymentLimits.java
+++ src/main/java/api/PaymentLimits.java
@@ -1,24 +1,25 @@
 package api;
 
-public class PaymentLimits extends PaymentSystemData {
-    private String minimum;
-    private String maximum;
-
-    public PaymentLimits(String paymentsSystem, Object limits) {
-        super(paymentsSystem, limits);
-    }
-
-    public PaymentLimits(String paymentsSystem, Object limits, String minimum, String maximum) {
-        super(paymentsSystem, limits);
-        this.minimum = minimum;
-        this.maximum = maximum;
-    }
-
-    public String getMinimum() {
-        return minimum;
-    }
-
-    public String getMaximum() {
-        return maximum;
-    }
+public class PaymentLimits  {
+//public class PaymentLimits extends PaymentSystemData {
+//    private String minimum;
+//    private String maximum;
+//
+//    public PaymentLimits(String paymentsSystem, Object limits) {
+//        super(paymentsSystem, limits);
+//    }
+//
+//    public PaymentLimits(String paymentsSystem, Object limits, String minimum, String maximum) {
+//        super(paymentsSystem, limits);
+//        this.minimum = minimum;
+//        this.maximum = maximum;
+//    }
+//
+//    public String getMinimum() {
+//        return minimum;
+//    }
+//
+//    public String getMaximum() {
+//        return maximum;
+//    }
 }
